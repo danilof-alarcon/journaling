05a18c0f65fd17bdbb807702dfdd18f2dcff8944
DANILOALARCON <danilof.alarcon@gmail.com>
2023-08-19 20:31:59 -0500

    Finishing the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material/styles'
-import Home from './pages/HomePage';
+import Journaling from './pages/JournalingPage';
 import theme from './utils/theme';
 
 
 function App() {
-
   return (
+
     <ThemeProvider theme={theme}>
       <Router>
         <Routes>
-          <Route path='/' element={<Home />}/>
+          <Route path='/' element={<Journaling />}/>
         </Routes>
       </Router>
     </ThemeProvider>
+
   )
 }
 
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -7,19 +7,6 @@ const theme = createTheme({
         main: '#000000',
         },
     },
-    components: {
-        MuiPaper: {
-        defaultProps: {
-            elevation: 0,
-        },
-        styleOverrides: {
-            root: {
-            boxShadow: '6px 6px 6px 0px rgba(0,0,0,0.1)',
-            borderRadius: "16px",
-            },
-        }
-        }
-    },
     typography: {
         fontFamily: [
         'Poppins',
